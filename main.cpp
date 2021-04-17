bdf5a48639b4431d18c3303b93a09268e08eeff2
GStork <>
2021-04-17 13:39:54 +0000

    Decodificação 2.

--- main.cpp
+++ main.cpp
@@ -12,17 +12,26 @@ int main() {
   int c;
   int d;
   int n;
+  int l;
   int r = 1;
   string z;
   string numbers = "0123456789";
   string converted_number = "ABCDEFGHIJ";
   string letter;
+  string repeated_letter;
+  string repeated_letter_number;
+  string repeated_letter_string;
+  int position;
   string method;
   
   cout << "'C' para codificar 'D' para decodificar: " ;
   cin >> method;
 
-  if(method == "C"){
+  if(method != "C" && method != "D"){
+    cout << "Comando inválido." << endl;
+    main();
+  }
+  else if(method == "C"){
     cout << "Digite a mensagem para codificação: ";
     cin >> text;
     transform(text.begin(), text.end(), text.begin(), ::toupper);
@@ -64,21 +73,39 @@ int main() {
   }
   else if(method == "D"){
     cout << "Digite a mensagem para decodificação: ";
-    cin >> text;
-    if(text.length() > 0 && text.length() < 1000){
-      for(c = 0; c <= text.length() ; d++){
+    cin >> coded_text;
+    transform(coded_text.begin(), coded_text.end(), coded_text.begin(), ::toupper);
+    if(coded_text.length() > 0 && coded_text.length() < 1000){
+      for(c = 0; c <= coded_text.length() ; c++){
         n = 0;
+        
         while(n < numbers.length()){
-          if(text[c] == numbers[n]){
+          
+          if(coded_text[c] == numbers[n]){
+            repeated_letter_string.clear();
+            repeated_letter = coded_text[c + 1];
+            repeated_letter_number = coded_text[c];
+            position = stoi(repeated_letter_number);
+            for(l = 0; l < position; l++){
+              repeated_letter_string.append(1, coded_text[c + 1]);
+            }
+
+
+            
+            
+            
+
+            break;
           }
+          else if(n == 9){
+            break;
+          }
+          n++;
         }
       }
     }
+    cout << "MENSAGEM DECODIFICADA: " << coded_text << endl;
+    cout << "MENSAGEM DECODIFICADA: " << repeated_letter_string << endl;
   }
 
-
 }
-
-//int find_repeated_letters(){
-
-//}
